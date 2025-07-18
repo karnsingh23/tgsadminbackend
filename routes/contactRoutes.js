a30289e71d9f169b62ef07c45c6051fea1025c79
Karan Singh <urskaransingh4@gmail.com>
2025-07-18 17:32:55 +0530

    Add files via upload

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,9 +1,24 @@
 import express from "express";
 import multer from "multer";
+import crypto from "crypto";
+import path from "path";
 import { submitContactForm } from "../controllers/contactController.js";
 
 const router = express.Router();
-const upload = multer({ dest: "uploads/" });
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = crypto.randomBytes(16).toString("hex");
+    const ext = path.extname(file.originalname); 
+    cb(null, uniqueName + ext);
+  },
+});
+
+const upload = multer({ storage });
 
 router.post("/", upload.array("files"), submitContactForm);
 
