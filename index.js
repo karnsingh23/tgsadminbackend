5a22edbc5b42592845d07c0357c56d681e215273
Karan Singh <urskaransingh4@gmail.com>
2025-07-21 09:12:48 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import contactRoutes from "./routes/contactRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 connectDB();
@@ -12,13 +18,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/api/contact", contactRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/",(req,res)=>{
-  res.send('server is running ')
-})
+
+app.use("/", (req, res) => {
+  res.send('server is running');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
