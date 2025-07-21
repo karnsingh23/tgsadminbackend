b7f9f3b025a967285a52e495b9aebea3d43f7eff
Karan Singh <urskaransingh4@gmail.com>
2025-07-21 09:11:51 +0530

    added cloudinary

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,24 +1,12 @@
+// routes/contactRoutes.js
 import express from "express";
 import multer from "multer";
-import crypto from "crypto";
-import path from "path";
+import { storage } from "../utils/cloudinary.js"; // NEW
 import { submitContactForm } from "../controllers/contactController.js";
 
 const router = express.Router();
 
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = crypto.randomBytes(16).toString("hex");
-    const ext = path.extname(file.originalname); 
-    cb(null, uniqueName + ext);
-  },
-});
-
-const upload = multer({ storage });
+const upload = multer({ storage }); // Use Cloudinary storage now
 
 router.post("/", upload.array("files"), submitContactForm);
 
